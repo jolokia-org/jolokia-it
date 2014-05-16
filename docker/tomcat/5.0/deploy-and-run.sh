1d8d178237ee3c369c54cc7aaa8d95a333f0e505
Roland Huß <roland@jolokia.org>
2014-05-16 21:26:58 +0200

    Updated images for new way of deploying with jolokia/docker-maven-plugin

---
+++ docker/tomcat/5.0/deploy-and-run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR=${DEPLOY_DIR:-/maven}
+echo "Checking *.war in $DIR"
+if [ -d $DIR ]; then
+  for i in $DIR/*.war; do
+     file=$(basename $i)
+     echo "Linking $i --> /opt/tomcat/webapps/$file"
+     ln -s $i /opt/tomcat/webapps/$file
+  done
+fi
+/opt/tomcat/bin/catalina.sh run
