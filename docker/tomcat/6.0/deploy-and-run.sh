9cfe4aa73dd978d9374debab5bfb07366d8f1ddf
Roland Huß <roland@jolokia.org>
2014-06-14 20:52:53 +0200

    Fixed run script

--- docker/tomcat/6.0/deploy-and-run.sh
+++ docker/tomcat/6.0/deploy-and-run.sh
@@ -10,5 +10,5 @@ if [ -d $DIR ]; then
   done
 fi
 # Use faster (though more unsecure) random number generator
-export CATATLINA_OPTS="-Djava.security.egd=file:/dev/./urandom"
+export CATALINA_OPTS="-Djava.security.egd=file:/dev/./urandom"
 /opt/tomcat/bin/catalina.sh run
